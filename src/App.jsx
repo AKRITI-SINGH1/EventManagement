230677d872a94bd024f907c929e7fe591c2e5e62
Akriti Singh <132009901+AKRITI-SINGH1@users.noreply.github.com>
2024-05-12 20:58:37 +0530

    event-manage-app
    Manage your events

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import { routes } from './routes/routes'
+
+
+const router = createBrowserRouter(routes)
+function App() {
+  
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App;
